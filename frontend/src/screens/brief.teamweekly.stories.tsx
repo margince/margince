51c952135a553f7eac796c4da0071bae7f0b31e9
Lars Jankowfsky <lars@gradion.com>
2026-09-13 18:50:21 +0700

    Provide the session in Home team-weekly stories [skip ci]

--- frontend/src/screens/brief.teamweekly.stories.tsx
+++ frontend/src/screens/brief.teamweekly.stories.tsx
@@ -5,10 +5,10 @@ import type { Meta, StoryObj } from "@storybook/react-vite";
 import { team, teamWeek } from "./brief.fixtures";
 import { TeamWeeklyPanel } from "./brief.teamweekly";
 import {
-  installFetchStub,
   jsonResponse,
   type RouteMap,
   StoryProviders,
+  stubWithSession,
 } from "./story-utils";
 
 // A team's frozen week, with the picker that chooses which team it is about.
@@ -38,7 +38,7 @@ function page(week: () => Response): RouteMap {
 
 function panel(routes: RouteMap) {
   return () => {
-    installFetchStub(routes);
+    stubWithSession(routes, {});
     return (
       <StoryProviders>
         <TeamWeeklyPanel offered />
